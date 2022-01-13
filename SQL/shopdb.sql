0e9c61582bfd217a649e212c3de05322b69351c5
jihye-12 <81624537+jihye-12@users.noreply.github.com>
2022-01-13 11:46:22 +0900

    Add files via upload

---
+++ SQL/shopdb.sql
@@ -0,0 +1,14 @@
+use shopdb; #자바 import
+
+insert into memberTBL values ('id1','이름1','주소1');
+insert into memberTBL values ('id2','이름2','주소2');
+insert into memberTBL values ('id3','이름3','주소3');
+
+SELECT * FROM memberTBL;
+
+insert into productTBL values ('상품1',10000,'2020-12-24','제조사1',20);
+insert into productTBL values ('상품2',20000,'1999-12-24','제조사2',10);
+insert into productTBL values ('상품3',30000,'2020-12-24','제조사1',30);
+insert into productTBL values ('상품4',40000,'2020-12-24','제조사2',40);
+
+SELECT * FROM productTBL;
