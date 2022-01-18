bbd80957aaf143c5fa4333a4b2f1063e391b228e
jihye-12 <81624537+jihye-12@users.noreply.github.com>
2022-01-18 17:19:16 +0900

    Add files via upload

---
+++ Study/B1152.java
@@ -0,0 +1,13 @@
+package test;
+
+import java.util.Scanner;
+
+public class B1152 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in); 
+		String S = sc.nextLine().trim(); 
+		System.out.println(S.split(" ").length); 
+		} 
+	}
+
