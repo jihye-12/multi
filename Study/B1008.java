9af543c8fbef925548bb75f5fc569945d762c935
jihye-12 <81624537+jihye-12@users.noreply.github.com>
2022-01-27 21:54:28 +0900

    Add files via upload

---
+++ Study/B1008.java
@@ -0,0 +1,16 @@
+package test;
+
+import java.util.Scanner;
+
+public class B1008 {
+
+	public static void main(String[] args) {
+		   Scanner sc = new Scanner(System.in);
+		   
+			int first = sc.nextInt();
+			int second = sc.nextInt();
+			
+			System.out.println(first/second);
+	}
+
+}
